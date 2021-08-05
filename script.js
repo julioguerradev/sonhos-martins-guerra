321cb4f5efbec9a44944d2d5259318ffa6706a7c
julioguerradev <76126901+julioguerradev@users.noreply.github.com>
2021-08-04 23:38:25 -0300

    commit terminando primeira página

---
+++ script.js
@@ -0,0 +1,12 @@
+function mouseOn(){
+    let label = document.getElementsByTagName("label")[0];
+    label.setAttribute("class","select-img");
+}
+function mouseOut(element){
+    let label = document.getElementsByTagName("label")[0];
+    label.className = "";
+}
+
+
+
+
