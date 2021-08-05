6bb676de858844cacac38c551336090a6bf77c56
julioguerradev <76126901+julioguerradev@users.noreply.github.com>
2021-08-05 17:01:02 -0300

    commit end first design

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function mouseOn(){
-    let label = document.getElementsByTagName("label")[0];
+    let label = document.getElementsByTagName("label")[1];
     label.setAttribute("class","select-img");
 }
 function mouseOut(element){
-    let label = document.getElementsByTagName("label")[0];
+    let label = document.getElementsByTagName("label")[1];
     label.className = "";
 }
 
-
+function darkMode(element){
+    document.getElementsByTagName("input");
+    let checkbox = document.getElementById("btnDarkMode");
+    let dark = document.body;
+    if (checkbox.checked){
+        dark.setAttribute("class","bright-mode");
+    }else{
+        dark.setAttribute("class","dark-mode");
+    }
+}
 
 
